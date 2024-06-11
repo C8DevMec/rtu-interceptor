b69277a5cf6d56c26901c52aa9c0d72b5d0fb3aa
C8DevMec <93236947+C8DevMec@users.noreply.github.com>
2024-06-11 14:32:38 +0800

    change to script; removal of api support

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
 from typing import List, Optional, Tuple
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
 from string import Template
+from loguru import logger
 
-import uvicorn
+import aiohttp
 import asyncio
 import os
 import requests
 import tomli
 import json
 import subprocess
-import multiprocessing
 
-
-# TODO: Add logging
-# TODO: Remove get_json function
-
+logger.add("app-interceptor.log", rotation="5 MB")
 load_dotenv()
 
 if os.path.exists("config.toml"):
@@ -27,79 +22,49 @@ else:
     raise FileNotFoundError("config.toml not found")
 
 
-class Date(BaseModel):
-    Value: str
-    Status: int
-
-
-class Point(BaseModel):
-    Name: str
-    Date: Date
-    Value: float
-    ValueRaw: int
-    Status: int
-
-
-class DataPoint(BaseModel):
-    Points: List[Point]
-    ChangeID: Optional[int] = None
-
-
-class SuccessResponse(BaseModel):
-    message: str
-    status: int
-
-
-app = FastAPI()
-
-
-@app.get("/api/v1/interceptor")
-async def interceptor() -> SuccessResponse:
-    asyncio.create_task(start_process())
-    return SuccessResponse(
-        message="Data was received and will be processed.", status=200
-    )
-
-
 async def start_process():
     rtu_data = await pull_data()
+    if not rtu_data:
+        logger.error("Failed to pull data from RTU.")
     with open("response.json", "w") as f:
         json.dump(rtu_data, f, indent=4)
-    filtered_data = await collect_forced_data(rtu_data)
+    filtered_data = collect_forced_data(rtu_data)
     for obj in filtered_data:
-        timestamp = convert_date(obj["Date"]["Value"])
-        created, config_path = await create_config(obj["Name"], timestamp, obj["Value"])
+        created, config_path = await create_config(
+            obj["Name"], obj["Date"]["Value"], obj["Value"]
+        )
         if not created:
-            raise Exception("Failed to create config file.")
+            logger.error("Failed to create config file.")
 
         if CONFIG["piconfig"]["push"]:
-            print("Pushing data to PI...")
+            logger.info("Pushing data to PI...")
             await run_piconfig(config_path)
 
 
 async def pull_data():
-    response = requests.post(
-        CONFIG["rtu"]["url"],
-        auth=(CONFIG["rtu"]["username"], CONFIG["rtu"]["password"]),
-        verify=False,
-    )
-    return response.json()
+    logger.info("Pulling data from RTU...")
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            CONFIG["rtu"]["url"],
+            auth=aiohttp.BasicAuth(
+                CONFIG["rtu"]["username"], CONFIG["rtu"]["password"]
+            ),
+            ssl=False,
+        ) as response:
+            logger.info(f"Response status: {response.status}")
+            if response.status != 200:
+                logger.error("Failed to pull data from RTU.")
+            return await response.json()
 
 
-async def collect_forced_data(data):
+def collect_forced_data(data) -> List[dict]:
     result = [
         obj
         for obj in data["Points"]
         if obj.get("Status") == CONFIG["rtu"]["status"]["forced"]
     ]
+    logger.info(f"Collected {len(result)} forced data.")
     return result
-
-
-#! TODO: REMOVE THIS FUNCTION
-def get_json() -> DataPoint:
-    with open("response.json") as file:
-        data = json.load(file)
-        return DataPoint(**data)
 
 
 async def create_config(tag: str, timestamp: str, value: float) -> Tuple[bool, str]:
@@ -107,6 +72,7 @@ async def create_config(tag: str, timestamp: str, value: float) -> Tuple[bool, s
     config_path = os.path.join(
         os.getcwd(), CONFIG["piconfig"]["config_folder"], f"{tag}.inp"
     )
+    original_timestamp, formatted_timestamp = convert_date(timestamp)
     PLACEHOLDER_VALUE = -1
     PLACEHOLDER_MODE = "replace"
     created = False
@@ -116,15 +82,20 @@ async def create_config(tag: str, timestamp: str, value: float) -> Tuple[bool, s
     with open(template_path, "r") as f:
         src = Template(f.read())
 
-    replace_content += f"{tag},{timestamp},{PLACEHOLDER_VALUE},{PLACEHOLDER_MODE}\n"
-    replace_content += f"{tag},{timestamp},{value},{PLACEHOLDER_MODE}"
+    replace_content += (
+        f"{tag},{formatted_timestamp},{PLACEHOLDER_VALUE},{PLACEHOLDER_MODE}\n"
+    )
+    replace_content += f"{tag},{formatted_timestamp},{value},{PLACEHOLDER_MODE}"
 
     result = src.substitute(
-        tag=tag, timestamp=timestamp, value=value, add_data=replace_content
+        tag=tag,
+        original_timestamp=original_timestamp,
+        value=value,
+        add_data=replace_content,
     )
 
     if not os.path.exists(CONFIG["piconfig"]["config_folder"]):
-        print("Creating configs folder...")
+        logger.info("Creating configs folder...")
         os.makedirs(CONFIG["piconfig"]["config_folder"])
 
     with open(config_path, "w") as f:
@@ -138,16 +109,16 @@ async def run_piconfig(config_path):
     command = subprocess.run(
         f"piconfig < {config_path}", shell=True, text=True, capture_output=True
     )
-    print(command.stdout)
+    logger.info(command.stdout)
 
 
-def convert_date(date_str):
+def convert_date(date_str) -> Tuple:
     date_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%fZ")
+    date_obj += timedelta(hours=8)  # UTC+8
     formatted_date = date_obj.strftime("%d-%b-%Y %H:%M:%S.%f")
 
-    return formatted_date
+    return date_str, formatted_date
 
 
 if __name__ == "__main__":
-    multiprocessing.freeze_support()  # For Windows support
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=False, workers=1)
+    asyncio.run(start_process())
